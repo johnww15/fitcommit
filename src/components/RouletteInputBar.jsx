0ae7ec65c4c5c02866048b04585ecc5b7f704cdf
jlimsy <limsy.joey@gmail.com>
2024-01-21 17:08:08 +0800

    changed roulette search by muscle group into a dropdown bar

--- src/components/RouletteInputBar.jsx
+++ src/components/RouletteInputBar.jsx
@@ -10,13 +10,16 @@ export default function RouletteInputBar() {
           Search
         </label>
 
-        <input
-          type="search"
-          id="default-search"
-          className="bg-zinc-50 border border-zinc-300 text-zinc-900 sm:text-sm rounded-lg focus:ring-jade-600 focus:border-jade-600 block w-full"
-          placeholder="e.g. chest"
-          required
-        />
+        <select
+          id="countries"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option selected>Choose a muscle group:</option>
+          <option value="Arms">Arms</option>
+          <option value="Back">Back</option>
+          <option value="Chest">Chest</option>
+          <option value="Legs">Legs</option>
+        </select>
 
         <button type="submit" className=" bg-jade-500 block w-full p-2.5">
           Search
