6ecf979c114827b4c2eaaca0b4393bef5b7d36bc
jlimsy <limsy.joey@gmail.com>
2024-01-22 13:16:35 +0800

    modified calender design

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,16 @@
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
+const handleClickDay = () => {
+  alert("day is clicked");
+};
 export default function CalendarCard() {
   return (
-    <div className="items-center justify-center px-6 py-8 mx-auto lg:py-0">
-      <Calendar />
+    <div className=" items-center justify-center px-6 py-8 mx-auto lg:py-0 w-1/2">
+      <Calendar
+        className="rounded-lg bg-zinc-800 justify-center text-center "
+        onClickDay={handleClickDay}
+      />
     </div>
   );
 }
