c910045c3765e75f82be1b47686cc7b0fa0b272c
jlimsy <limsy.joey@gmail.com>
2024-01-23 13:47:31 +0800

    feat: modal when add to calendar is clicked

--- src/components/FavouritesCard.jsx
+++ src/components/FavouritesCard.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
 export default function FavouritesCard() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleRemove = () => {
+    alert("remove from favourites");
+  };
+
   return (
     <>
       <div className="flex flex-col rounded-lg text-sm px-5 py-2.5 m-2 bg-zinc-800 border border-jade-700">
@@ -11,43 +24,21 @@ export default function FavouritesCard() {
           <p>Category: Chest</p>
         </div>
         <div className="grid grid-cols-2 px-6 pt-4 pb-2 gap-5">
-          <button className="bg-jade-500 px-1.5 py-1">+ Calendar</button>
-          <button className="bg-zinc-500 text-zinc-200 hover:bg-zinc-600 px-1.5 py-1">
+          <button className="bg-jade-500 px-1.5 py-1" onClick={handleClick}>
+            + Calendar
+          </button>
+          <button
+            className="bg-zinc-500 text-zinc-200 hover:bg-zinc-600 px-1.5 py-1"
+            onClick={handleRemove}
+          >
             Remove
           </button>
-        </div>
-      </div>
-      <div className="flex flex-col rounded-lg text-sm px-5 py-2.5 m-2 bg-zinc-800 border border-jade-700">
-        <img
-          className="object-cover h-32 w-full"
-          src="https://images.unsplash.com/photo-1517836357463-d25dfeac3438?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Exercise Name</div>
-          <p>Category: Chest</p>
-        </div>
-        <div className="grid grid-cols-2 px-6 pt-4 pb-2 gap-5">
-          <button className="bg-jade-500 px-1.5 py-1">+ Calendar</button>
-          <button className="bg-zinc-500 text-zinc-200 hover:bg-zinc-600 px-1.5 py-1">
-            Remove
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col rounded-lg text-sm px-5 py-2.5 m-2 bg-zinc-800 border border-jade-700">
-        <img
-          className="object-cover h-32 w-full"
-          src="https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Exercise Name</div>
-          <p>Category: Chest</p>
-        </div>
-
-        <div className="grid grid-cols-2 px-6 pt-4 pb-2 gap-5">
-          <button className="bg-jade-500 px-1.5 py-1">+ Calendar</button>
-          <button className="bg-zinc-500 text-zinc-200 hover:bg-zinc-600 px-1.5 py-1">
-            Remove
-          </button>
+          {showModal && (
+            <>
+              <Modal setShowModal={setShowModal} />
+              <div className="opacity-25 fixed inset-0 z-40 bg-zinc-50"></div>
+            </>
+          )}
         </div>
       </div>
     </>
--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FavouritesCard from "../../components/FavouritesCard";
 
 export default function FavouritesPage() {
