41bf1ff649427efe7ec74fdb2b180f9ebad9366e
Binjobo <101441601+Binjobo@users.noreply.github.com>
2024-01-24 02:12:33 +0800

    feat:make roulette items searchable by muscle group

--- src/pages/RoulettePage/RoulettePage.jsx
+++ src/pages/RoulettePage/RoulettePage.jsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+// import { useState } from "react";
 import RouletteInputBar from "../../components/RouletteInputBar";
 import RouletteResults from "../../components/RouletteResults";
 import exercisesByMuscle from "../../assets/exercisesByMuscle";
 
-export default function RoulettePage() {
-  const [rouletteResults, setRouletteResults] = useState("");
+export default function RoulettePage({ selectedMuscle, onMuscleChange }) {
+  // const [rouletteResults, setRouletteResults] = useState("");
 
   const fetchByMuscle = exercisesByMuscle.filter(
-    (item) => item.muscle === "Legs"
+    (item) => item.muscle === selectedMuscle
   );
   console.log(fetchByMuscle);
 
   return (
     <section className="items-center justify-center px-6 py-8 mx-auto lg:py-0">
-      <RouletteInputBar />
+      <RouletteInputBar
+        selectedMuscle={selectedMuscle}
+        onSearch={onMuscleChange}
+      />
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
         {fetchByMuscle.map((item, idx) => (
           <RouletteResults key={idx} item={item} />
