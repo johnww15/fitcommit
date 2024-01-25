fac597c7b6f3484eea926aed8a9f7e43211bf76f
Binjobo <101441601+Binjobo@users.noreply.github.com>
2024-01-25 23:11:13 +0800

    testing

--- src/components/EntriesCard.jsx
+++ src/components/EntriesCard.jsx
@@ -7,36 +7,37 @@ export default function EntriesCard({ entry, user, setUser }) {
     });
     return formattedDate;
   };
-  console.log("user", user);
+  //   console.log("user", user);
 
-  const pastFiveEntries = user?.entry
-    .slice(0, 5) // Get the first 5 entries
-    .filter((entry) => new Date(entry.date) <= new Date());
+  //   const pastFiveEntries = user?.entry
+  //     .slice(0, 5) // Get the first 5 entries
+  //     .filter((entry) => new Date(entry.date) <= new Date());
+
+  //   return (
+  //     <>
+  //       {pastFiveEntries.map((pastEntry) => (
+  //         <div
+  //           key={pastEntry.date}
+  //           className="rounded-lg text-sm px-5 py-2.5 mb-2 bg-zinc-800 border border-jade-700"
+  //         >
+  //           <h1>{formatDate(pastEntry.date)}</h1>
+  //           <p>
+  //             {pastEntry.exercise} - {pastEntry.weight}kg
+  //           </p>
+  //         </div>
+  //       ))}
+  //     </>
+  //   );
+  // }
 
   return (
     <>
-      {pastFiveEntries.map((pastEntry) => (
-        <div
-          key={pastEntry.date}
-          className="rounded-lg text-sm px-5 py-2.5 mb-2 bg-zinc-800 border border-jade-700"
-        >
-          <h1>{formatDate(pastEntry.date)}</h1>
-          <p>
-            {pastEntry.exercise} - {pastEntry.weight}kg
-          </p>
-        </div>
-      ))}
+      <div className="rounded-lg text-sm px-5 py-2.5 mb-2 bg-zinc-800 border border-jade-700">
+        <h1>{formatDate(entry.date)}</h1>
+        <p>
+          {entry.exercise} - {entry.weight}kg
+        </p>
+      </div>
     </>
   );
 }
-
-// return (
-//   <>
-//     <div className="rounded-lg text-sm px-5 py-2.5 mb-2 bg-zinc-800 border border-jade-700">
-//       <h1>{formatDate(entry.date)}</h1>
-//       <p>
-//         {entry.exercise} - {entry.weight}kg
-//       </p>
-//     </div>
-//   </>
-// );
