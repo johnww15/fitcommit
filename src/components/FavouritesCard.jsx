5356048bca9dd10788a321284f2f35ab65b9ddfe
Binjobo <101441601+Binjobo@users.noreply.github.com>
2024-01-25 11:50:44 +0800

    feat:pulling from database for favouritepage now

--- src/components/FavouritesCard.jsx
+++ src/components/FavouritesCard.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import AddToCalendarModal from "./AddToCalendarModal";
 
-export default function FavouritesCard({ favorites }) {
+export default function FavouritesCard({ favorites, handleRemove }) {
   const [showModal, setShowModal] = useState(false);
 
   const handleClick = () => {
     setShowModal(true);
   };
 
-  const handleRemove = () => {
-    alert("remove from favourites");
-  };
+  // const handleRemove = () => {
+  //   alert("remove from favourites");
+  // };
 
   return (
     <>
       <div className="flex flex-col rounded-lg text-sm px-5 py-2.5 m-2 bg-zinc-800 border border-jade-700">
+        <div>{favorites._id}</div>
         <img className="object-cover h-32 w-full" src={favorites.img} />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{favorites.name}</div>
+          <div className="font-bold text-xl mb-2">{favorites.exercise}</div>
           <p>{favorites.muscle}</p>
         </div>
         <div className="grid grid-cols-2 px-6 pt-4 pb-2 gap-5">
--- src/pages/FavouritesPage/FavouritesPage.jsx
+++ src/pages/FavouritesPage/FavouritesPage.jsx
@@ -2,48 +2,64 @@ import { useState, useEffect } from "react";
 import FavouritesCard from "../../components/FavouritesCard";
 import { getFavouriteByUserId } from "../../utilities/favourites-service";
 
-export default function FavouritesPage({ favorites }) {
+export default function FavouritesPage() {
+  const [favItems, setFavItems] = useState([]);
+
   useEffect(() => {
     (async function () {
       const response = await getFavouriteByUserId();
-      console.log(response);
+      console.log("testing", response);
+      setFavItems(response.favourites);
     })();
   }, []);
+
+  //can remove it since you already have it elsewhereg
+  const BASE_URL = "/api/favourites";
+
+  const handleDelete = async (_id) => {
+    const response = await fetch(BASE_URL + `/${_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // Show pop-up message
+    window.alert("Removed from Favourites!");
+
+    if (response.ok) {
+      setFavItems((prevFavs) =>
+        prevFavs.filter((favorites) => favorites._id !== _id)
+      );
+    }
+  };
+
+  console.log(favItems);
 
   return (
     <section className="items-center justify-center px-6 py-8 mx-auto lg:py-0">
       <h1>My Saved Workouts</h1>
 
-      {favorites.map((favorites, idx) => (
+      {favItems.map((favorites, idx) => (
         <div key={idx} className="flex flex-col md:flex-row">
-          <FavouritesCard favorites={favorites} />
+          <FavouritesCard
+            favorites={favorites}
+            onDelete={() => handleDelete(favorites._id)}
+          />
         </div>
       ))}
     </section>
   );
 }
 
-// export default function FavouritesPage({ favorites }) {
 //   return (
 //     <section className="items-center justify-center px-6 py-8 mx-auto lg:py-0">
 //       <h1>My Saved Workouts</h1>
 
-//       <div className="flex flex-col md:flex-row">
-//         {favorites.map((favorite, idx) => (
-//           <div
-//             key={idx}
-//             className="flex flex-col rounded-lg px-5 py-2.5 mb-2 bg-zinc-800 border border-jade-700"
-//           >
-//             <img src={favorite.img} className="object-cover h-32 w-full" />
-//             <div className="px-6 py-4">
-//               <p>{favorite.muscle}</p>
-//               <h1>{favorite.name}</h1>
-//             </div>
-//             {/* Add to Calendar and Add to Favorites buttons go here */}
-//             {/* Display favorite item details here */}
-//           </div>
-//         ))}
-//       </div>
+//       {favorites.map((favorites, idx) => (
+//         <div key={idx} className="flex flex-col md:flex-row">
+//           <FavouritesCard favorites={favorites} />
+//         </div>
+//       ))}
 //     </section>
 //   );
 // }
